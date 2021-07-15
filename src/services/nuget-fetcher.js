ab6d96588520fbfb4cc5d4a6640a3fc5a5c9f38f
Tim Tse <timothy.tse@quadpay.com>
2021-07-15 12:32:38 -0400

    added error checking to nuget-fetcher

--- src/services/nuget-fetcher.js
+++ src/services/nuget-fetcher.js
@@ -5,34 +5,53 @@ const BASE_URL = 'https://api.nuget.org/v3/registration5-gz-semver2/';
 
 const getLatestVersion = async (packageName) => {
     const url = BASE_URL + packageName.toLowerCase() + '/index.json';
-    const data = await fetch(url).then(res => res.json());
 
-    const items = data.items;
-    var highestVersion = '0.0.0';
+    try{
+        const data = await fetch(url).then(res => res.json());
 
-    items.forEach(item => {
-        var maxVersion = item.upper;
-        if(compareVersions(maxVersion, highestVersion) === 1){
-            highestVersion = maxVersion;
+        const items = data.items;
+        var highestVersion = '0.0.0';
+
+        items.forEach(item => {
+            var maxVersion = item.upper;
+            if(compareVersions(maxVersion, highestVersion) === 1){
+                highestVersion = maxVersion;
+            }
+        });
+
+        return{
+            status: 200,
+            version: highestVersion
         }
-    });
-
-    return highestVersion;
-
+    }catch(err){
+        return {
+            status: 500,
+            message: err
+        }
+    }
 };
 
 exports.isUpToDate = async (packageName, version) => {
     var latest = await getLatestVersion(packageName);
 
-    if(compareVersions(latest, version) === 1){
+    if(latest.status !== 200){
+        return{
+            status: latest.status,
+            message: latest.message
+        }
+    }
+
+    if(compareVersions(latest.version, version) === 1){
         return {
+            status: 200,
             upToDate: false,
-            latestVersion: latest,
+            latestVersion: latest.version,
             installed: version
         }
     }
 
     return {
+        status: 200,
         upToDate: true,
     };
 };
