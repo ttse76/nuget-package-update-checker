09427679efb4f3e3026a033f664bf785d81c6a8b
Tim Tse <timothy.tse@quadpay.com>
2021-07-16 11:28:07 -0400

    added loop to evaluate multiple packages at once

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const nuget = require('./services/nuget-fetcher');
 const main = async () => {
     var fileloc = {};
     var fileLocLabels = [];
+
     try{
         fileloc = require('../config/fileloc.json');
         fileLocLabels = Object.keys(fileloc);
@@ -12,27 +13,28 @@ const main = async () => {
         console.error('Error: No fileloc.json file found');
         return;
     }
-
-    console.log('Evaluating packages for ' + label + '...');
-    const path = fileloc[label];
-    const data = await parser.getInstalledVersions(path);
-    if(data.status !== 200){
-        console.error('Error for project ' + label + ': '  + data.message);
-        return;
-    }
-
-    const packages = data.packages;
-    const packageNames = Object.keys(packages);
-
-    packageNames.forEach(async package => {
-        const version = packages[package];
-        const res = await nuget.isUpToDate(package, version);
-        if(res.status !== 200){
-            console.log('Error in version check for ' + package + ': ' + res.message);
+    fileLocLabels.forEach(async label => {
+        console.log('Evaluating packages for ' + label + '...');
+        const path = fileloc[label];
+        const data = await parser.getInstalledVersions(path);
+        if(data.status !== 200){
+            console.error('Error for project ' + label + ': '  + data.message);
             return;
         }
-        var outStr = package + ',' + res.installed + ',' + res.latestVersion + '\n';
-        fs.appendFile(label + '.packages.txt', outStr, err => {});
+
+        const packages = data.packages;
+        const packageNames = Object.keys(packages);
+
+        packageNames.forEach(async package => {
+            const version = packages[package];
+            const res = await nuget.isUpToDate(package, version);
+            if(res.status !== 200){
+                console.log('Error in version check for ' + package + ': ' + res.message);
+                return;
+            }
+            var outStr = package + ',' + res.installed + ',' + res.latestVersion + '\n';
+            fs.appendFile(label + '.packages.txt', outStr, err => {});
+        });
     });
     return;
 }
